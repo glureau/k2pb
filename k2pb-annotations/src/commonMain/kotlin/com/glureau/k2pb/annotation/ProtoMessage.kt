ab500980e0688261f7931a395fb5838d34492c08
Grégory Lureau <gregory.lureau@sumup.com>
2025-04-22 17:56:43 +0200

    0.9.15: Allow ProtoMessage annotation on typealias (tmp workaround)

--- k2pb-annotations/src/commonMain/kotlin/com/glureau/k2pb/annotation/ProtoMessage.kt
+++ k2pb-annotations/src/commonMain/kotlin/com/glureau/k2pb/annotation/ProtoMessage.kt
@@ -2,8 +2,11 @@ package com.glureau.k2pb.annotation
 
 /**
  * Compared to Ktx Serialization, this annotation has no link check (yet).
+ *
+ * AnnotationTarget.TYPEALIAS is usable for migration tricks.
+ * A more suitable solution needs to be defined instead of that trick.
  */
-@Target(AnnotationTarget.CLASS)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPEALIAS)
 public annotation class ProtoMessage(
     val name: String = ""
 )
